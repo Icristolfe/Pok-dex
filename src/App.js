492fac017e6d2c4bc8f1320712ddec1cc3dcaa9e
Cristolfe <cristolfeicaro@gmail.com>
2022-05-24 17:36:01 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { api } from './services/api'
+
+import Pokebola from './assets/pokebola.png'
+
+import {
+  Body,
+  ContainerItems,
+  Info,
+  Loading,
+  Result,
+  InfoImage
+} from './style'
 
 function App() {
+
+  const [pokemon, setPokemon] = useState(null)
+  const [error, setError] = useState(null)
+  const [typedPokemon, setTypedPokemon] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleChange = (event) => {
+    setTypedPokemon(event.target.value)
+
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    if (!typedPokemon) {
+      return
+    }
+    setIsLoading(true)
+    try {
+      const response = await api.get(`/pokemon/${typedPokemon}`)
+      setPokemon(response.data)
+      setError(null)
+      setIsLoading(false)
+    }
+    catch (error) {
+      setError('pokemon não encontrado')
+      setIsLoading(false)
+      setPokemon(null)
+    }
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Body>
+      <ContainerItems>
+        <h1>Seja bem vindo à Pokedex!</h1>
+
+        <p>Digite o nome de um pokemon ou Id para começar</p>
+
+        <form onSubmit={handleSubmit}>
+
+          <input
+            value={typedPokemon}
+            placeholder='Nome do pokemon/id'
+            onChange={handleChange}
+          />
+
+          <button type='submit'>
+            {
+              isLoading ? (<Loading />)
+                : (<>
+                  Buscar
+                </>)
+            }
+            <img src={Pokebola} alt="pokebola" ></img>
+          </button>
+
+        </form>
+
+        <Result>
+          {
+            pokemon && (
+              <div key={pokemon.id}>
+                {isLoading ? (
+                  error
+                ) : (
+                  <>
+                    <InfoImage>
+                      <h2>{pokemon.name}</h2>
+                      <img src={pokemon.sprites['front_default']} alt={pokemon.name} />
+
+                    </InfoImage>
+
+                    <Info>
+                      <span>
+                        <strong>Height</strong>: {pokemon.height * 10}Cm
+                      </span>
+
+                      <span>
+                        <strong>Weight</strong>: {pokemon.weight * 10}Kg
+                      </span>
+
+                      <span>
+                        <strong>Type</strong>: {pokemon.types[0].type.name}
+                      </span>
+
+                      <span>
+                        <strong>Id</strong> : {pokemon.id}
+                      </span>
+
+
+
+                    </Info>
+                  </>
+                )
+                }
+
+              </div>
+            )
+          }
+
+        </Result>
+
+
+      </ContainerItems>
+    </Body>
+
   );
 }
 
---
+++ src/style.js
@@ -0,0 +1,165 @@
+import styled from 'styled-components'
+
+export const Body = styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+gap:10px;
+background: #0E0E0E;
+width:100vw;
+height:100vh;
+
+form{
+    display:flex;
+    flex-direction:row;
+
+}
+
+
+`
+
+export const ContainerItems = styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+padding-top:100px;
+background:#2F2F2F;
+width:800px;
+height:100vh;
+justify-content:start;
+
+
+h1 {
+    font-size: 50px;
+    color:#FFFFFF;
+    margin-bottom:50px;
+}
+p {
+    color:#FFFFFF;
+    font-size:25px;
+    margin-bottom:80px;
+}
+
+input {
+    width:300px;
+    height:50px;
+    display:flex;
+    border:none;
+    padding-left:10px;
+    border-bottom-left-radius:10px;
+    border-top-left-radius:10px;
+    
+
+}
+
+input:focus{
+    box-shadow: 0 0 0 0;
+    border: 0 none;
+    outline: 0;
+} 
+
+
+
+button {
+    width:200px;
+    height:50px;
+    display:flex;
+    flex-direction:row;
+    align-items:center;
+    justify-content:center;
+    gap:30px;
+    border:none;
+    color:#FFFFFF;
+    font-size:20px;
+    background:#E3350C;
+    cursor:pointer;
+    margin-bottom:50px;
+    border-top-right-radius:10px;
+    border-bottom-right-radius:10px;
+}
+
+button:hover {
+    opacity:0.9;
+}
+
+button:active {
+    opacity:0.6;
+}
+
+span {
+    font-size:20px;
+}
+`
+
+
+export const Info = styled.div`
+display:flex;
+flex-direction:row;
+align-items:center;
+justify-content:center;
+gap:10px;
+margin-bottom:50px;
+
+img {
+    width:100px;
+    height:100px;
+}
+`
+
+export const Loading = styled.div`
+height:25px;
+width:25px;
+border-top:4px solid #FFF;
+border-right: 4px solid transparent;
+border-radius:50%;
+animation: 0.5s spin linear infinite;
+
+@keyframes spin {
+    from {
+        transform:rotate(0deg);
+    }
+
+    to {
+        transform:rotate(360deg)
+    }
+}
+
+`
+
+export const Result = styled.div `
+width: 500px;
+height: 430px;
+background: rgba( 255, 255, 255, 0.25 );
+box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+backdrop-filter: blur( 13.5px );
+-webkit-backdrop-filter: blur( 13.5px );
+border-radius: 10px;
+border: 1px solid rgba( 255, 255, 255, 0.18 );
+display:flex;
+flex-direction:column;
+align-items:center;
+color:#EEE;
+padding-top:25px;
+font-size:18px;
+
+h2 {
+    font-size:40px;
+    font-weight:500;
+}
+
+img {   
+    margin-top: 55px;
+    margin-bottom: 30px;
+    width: 175px;
+    height: 175px;
+}
+
+
+`
+
+export const InfoImage = styled.div `
+display:flex;
+flex-direction:column;
+align-items:center;
+justify-content:center;
+`
