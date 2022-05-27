6bb6ee8bac7a183d0e607a24587970b96073ec83
Cristolfe <cristolfeicaro@gmail.com>
2022-05-27 11:23:22 -0300

    result with 375

--- src/style.js
+++ src/style.js
@@ -126,7 +126,7 @@ animation: 0.5s spin linear infinite;
 `
 
 export const Result = styled.div`
-width: 400px;
+width: 375px;
 height: 260px;
 background: rgba( 255, 255, 255, 0.25 );
 box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
